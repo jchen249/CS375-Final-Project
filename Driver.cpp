ce3c4a825888ff6b74ac847c33a0049621e295ba
alexwhitney90 <alexwhitney90@gmail.com>
2018-11-27 18:19:33 -0500

    added test case 3

--- Driver.cpp
+++ Driver.cpp
@@ -4,6 +4,10 @@
 
 int main(int argc, char **argv)
 {
+	//srand(time(null));
+	
+	//load factor = >75%
+	
 	Hash h1 = Hash(29);
 	Hash h2 = Hash(29);
 	Hash h3 = Hash(29);
@@ -31,10 +35,12 @@ int main(int argc, char **argv)
 	}
 	std::cout << "double hashing # of collisions: " << h3.getCollision() << std::endl;
 	
+	//load factor = 50-75%
+	
 	Hash h4 = Hash(47);
 	Hash h5 = Hash(47);
 	Hash h6 = Hash(47);
-	std:vector<int> v2;
+	std::vector<int> v2;
 	int r2;
 	for(int i=0; i<30; i++)
 	{
@@ -58,9 +64,35 @@ int main(int argc, char **argv)
 	}
 	std::cout << "double hashing # of collisions: " << h6.getCollision() << std::endl;
 	
+	//load factor = <50%
+	
+	Hash h7 = Hash(71);
+	Hash h8 = Hash(71);
+	Hash h9 = Hash(71);
+	std::vector<int> v3;
+	int r3;
+	for(int i=0; i<25; i++)
+	{
+		r3 = rand() % 100 + 1;
+		v3.push_back(r3);
+		std::cout << r3 << std::endl;
+	}
+	for(unsigned int i=0; i<v3.size(); i++)
+	{
+		h7.insert(v3[i], 0);
+	}
+	std::cout << "linear probing # of collisions: " << h7.getCollision() << std::endl;
+	for(unsigned int i=0; i<v3.size(); i++)
+	{
+		h8.insert(v3[i], 1);
+	}
+	std::cout << "quadratic probing # of collisions: " << h8.getCollision() << std::endl;
+	for(unsigned int i=0; i<v3.size(); i++)
+	{
+		h9.insert(v3[i], 2);
+	}
+	std::cout << "double hashing # of collisions: " << h9.getCollision() << std::endl;
+	
+	
 	return 0;
 }
-
-#include "Hash.h"
-#include <stdlib.h>
-#include <iostream>
